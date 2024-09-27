85fac3543fb38e45528fce6f48dcf8610bbce8b1
robertwaweru101 <robertwaweru101@gmail.com>
2024-09-27 09:08:57 +0300

    developed a passenger counting site

---
+++ FILE/index.js
@@ -0,0 +1,17 @@
+let count = 0
+
+let saveEl = document.getElementById("save-el");
+
+let countEl = document.getElementById("count-el");
+
+function increment () {
+    count += 1;
+    countEl.textContent = count;
+}
+function save() {
+    let countstr = count + "-";
+    saveEl.textContent += countstr;
+    countEl.textContent = 0;
+    count = 0;
+}
+console.log("let's count people on the subway!")
